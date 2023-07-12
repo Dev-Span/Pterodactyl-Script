33686b138fddd60334dccd491290fffe4747e7d4
Dev_Span <67538314+Dev-Span@users.noreply.github.com>
2023-07-12 20:08:20 +0200

    Update install.sh
    Signed-off-by: Dev_Span <67538314+Dev-Span@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -220,7 +220,7 @@ install_pterodactyl() {
     mkdir -p /var/www/pterodactyl
     cd /var/www/pterodactyl || exit
     if [ ${PANEL} = "latest" ]; then
-        curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz
+        curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/download/v1.7.0/panel.tar.gz
     else
         curl -Lo panel.tar.gz https://github.com/pterodactyl/panel/releases/download/${PANEL}/panel.tar.gz
     fi
